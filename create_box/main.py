e4653a08bcf4ad07ae69c64d838bda4d83099eb3
Shamika Goddard <sg3147@utsnyc.edu>
2017-01-14 01:59:43 +0000

    Initial commit

--- create_box/main.py
+++ create_box/main.py
@@ -2,7 +2,12 @@
 
 
 def create_box(height, width, character):
-    pass
+    if height >= 1 and width >=1:
+        box = ''
+        for row in range(height):
+            box = box + (character * width) + '\n'
+    print (box)
+    return box
 
 
 if __name__ == '__main__':
